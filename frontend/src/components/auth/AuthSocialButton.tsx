7e43da299777a9513d703c2219801e116287766e
jubairalamdev <jubairalam.dev@gmail.com>
2026-08-25 01:49:54 +0600

    feat: github and google login authentication added.

--- frontend/src/components/auth/AuthSocialButton.tsx
+++ frontend/src/components/auth/AuthSocialButton.tsx
@@ -1,45 +1,22 @@
 "use client";
 
-import { Button } from "@heroui/react";
-import { authClient } from "../../lib/auth-client";
-import { FcGoogle } from "react-icons/fc";
+import GoogleAuthButton from "./social/GoogleAuthButton";
+import GithubAuthButton from "./social/GithubAuthButton";
+import type { AuthMode } from "./auth.types";
 
 interface AuthSocialButtonProps {
+  mode?: AuthMode;
   isPending?: boolean;
 }
 
-
 export default function AuthSocialButton({
+  mode = "signin",
   isPending = false,
 }: AuthSocialButtonProps) {
-  const handleGoogleSignIn = async () => {
-    await authClient.signIn.social({
-      provider: "google",
-      callbackURL: "/",
-    });
-  };
-
   return (
-    <Button
-      type="button"
-      variant="outline"
-      fullWidth
-      isPending={isPending}
-      onPress={handleGoogleSignIn}
-      className="
-        h-10
-        rounded-lg
-        border-border
-        bg-transparent
-        text-sm
-        font-medium
-        text-foreground
-        shadow-none
-        hover:bg-muted
-      "
-    >
-      <FcGoogle className="text-2xl" />
-      <span>Sign up with Google</span>
-    </Button>
+    <div className="space-y-3 flex flex-col gap-3 md:flex-row">
+      <GoogleAuthButton mode={mode} isPending={isPending} />
+      <GithubAuthButton mode={mode} isPending={isPending} />
+    </div>
   );
 }
