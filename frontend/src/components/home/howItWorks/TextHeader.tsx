4be46dc26c9b802fccc6cb71a94284da465e859a
tayabunn <tayabunnesa@gmail.com>
2026-09-02 20:42:22 +0600

    Changed the font into bold

--- frontend/src/components/home/howItWorks/TextHeader.tsx
+++ frontend/src/components/home/howItWorks/TextHeader.tsx
@@ -2,12 +2,12 @@
 
 export default function TextHeader() {
   return (
-    <div className="w-full">
+    <div className="w-full font-bold">
       <h2 className="section-title">How to Start in <span className="text-brand">
               4 Steps?
               </span>
               </h2>
-      <p className="section-subtitle mt-1">
+      <p className="section-subtitle max-w-max mt-1">
         Master your career path with a dynamic AI-driven learning and
         verification framework.
       </p>
