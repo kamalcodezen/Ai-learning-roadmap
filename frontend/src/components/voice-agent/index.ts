752542662a69bae0cbeae0bf4e9dfb7dcf5ddb10
kamalcodezen <kamaluddin7908@gmail.com>
2026-09-16 09:40:38 +0530

    feat(voice-agent): add VoiceLiveModal, VoiceVisualizerOrb, VoiceControlBar components and related types

---
+++ frontend/src/components/voice-agent/index.ts
@@ -0,0 +1,8 @@
+export { VoiceLiveModal } from "./components/VoiceLiveModal";
+export { VoiceVisualizerOrb } from "./components/VoiceVisualizerOrb";
+export { VoiceControlBar } from "./components/VoiceControlBar";
+export { useVoiceConversation } from "./hooks/useVoiceConversation";
+export { useInlineVoiceChat } from "./hooks/useInlineVoiceChat";
+export { useSpeechRecognition } from "./hooks/useSpeechRecognition";
+export { useSpeechSynthesis } from "./hooks/useSpeechSynthesis";
+export type * from "./types";
